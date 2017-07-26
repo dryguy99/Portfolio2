a7b32d3b2acb9b62460fe1cf3dd1be707471a4b5
Timothy Dry <timothy.dry@verizon.net>
2017-07-26 13:45:05 -0400

    initial upload

---
+++ public/java/app.js
@@ -0,0 +1,6 @@
+var module = angular.module('flip1', []);
+module.controller('flipCtrl1', function($scope) {});
+var module1 = angular.module('flip2', []);
+module.controller('flipCtrl2', function($scope) {});
+var module2 = angular.module('flip3', []);
+module.controller('flipCtrl3', function($scope) {});
