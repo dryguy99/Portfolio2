ff553b1e08d06b692a4fb8c2b6d155b1be2d1be3
Timothy Dry <timothy.dry@verizon.net>
2017-07-29 16:40:11 -0400

    setup server to send email

---
+++ server.js
@@ -0,0 +1,78 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var nodemailer = require('nodemailer');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+var router = express.Router();// get an instance of the express Router
+// fix cors errors
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    //console.log('Something is happening.');
+    next();
+});
+
+router.use(express.static(__dirname + '/public'));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(methodOverride("_method"));
+
+// set up emailer =====================================================
+var transporter = nodemailer.createTransport({
+  service: 'AOL',
+  auth: {
+    user: 'timothy.dry@verizon.net',
+    pass: ''
+  }
+});
+
+var mailOptions = {
+  from: 'timothy.dry@verizon.net',
+  to: 'timothy.dry@verizon.net',
+  subject: 'sending message from my portfolio using node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+// Routes =============================================================
+
+
+app.use('/', router);
+
+
+// Starting our express app
+app.listen(PORT, function() {
+	 console.log("App listening on PORT " + PORT);
+});
